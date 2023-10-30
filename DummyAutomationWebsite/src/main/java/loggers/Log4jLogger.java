a8cc1fbda7a3cdd0c8e08ab62e13c3ed2a6e12cd
jones <jones@host.docker.internal>
2023-10-30 14:30:42 -0400

    Added dependencies and created Log4jLogger wrapper class

---
+++ DummyAutomationWebsite/src/main/java/loggers/Log4jLogger.java
@@ -0,0 +1,43 @@
+package loggers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jLogger implements ILogger {
+
+	private Logger log4jLogger;
+	
+	public Log4jLogger(String className) {
+		this.log4jLogger = LogManager.getLogger(className);
+	}
+	
+	@Override
+	public void trace(String message) {
+		this.log4jLogger.trace(message);
+	}
+
+	@Override
+	public void debug(String message) {
+		this.log4jLogger.debug(message);
+	}
+
+	@Override
+	public void info(String message) {
+		this.log4jLogger.info(message);
+	}
+
+	@Override
+	public void warn(String message) {
+		this.log4jLogger.warn(message);
+	}
+
+	@Override
+	public void error(String message) {
+		this.log4jLogger.error(message);
+	}
+
+	@Override
+	public void fatal(String message) {
+		this.log4jLogger.fatal(message);
+	}
+}
